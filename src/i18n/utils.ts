49eb3f6c9cf223fa0f4af923cb64a7bfc742fa9d
winetree94 <winetree94@gmail.com>
2024-07-02 21:25:46 +0900

    update

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,8 +1,16 @@
-import { languages, defaultLang } from './ui';
+import { languages, defaultLangCode } from './ui';
+
+export const getCurrentLangCode = (url: URL) => {
+    const [, lang] = url.pathname.split('/');
+    if (lang in languages && lang !== defaultLangCode) {
+        return lang as keyof typeof languages;
+    }
+    return defaultLangCode;
+}
 
 export function getLangAbsolutePath(url: URL) {
     const [, lang] = url.pathname.split('/');
-    if (lang in languages && lang !== defaultLang) {
+    if (lang in languages && lang !== defaultLangCode) {
         return '/' + lang as keyof typeof languages;
     }
     return '';
