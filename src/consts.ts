377201d109ecd6b1ae847ab73cb82bf37d613c80
winetree94 <winetree94@gmail.com>
2024-07-02 20:29:46 +0900

    feat: localize homepage constants and copy

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,6 @@
 export const SITE_TITLE = 'winetree94';
 export const SITE_DESCRIPTION = 'winetree94 world';
 export const GITHUB_URL = 'https://github.com/winetree94';
+export const NPM_URL = 'https://www.npmjs.com/~winetree94';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/hansol-park-995380265/';
 export const EMAIL = 'winetree94@gmail.com';
