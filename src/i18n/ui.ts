aa3407c27fe38f49b5e305d521c3cb70c4df49fb
winetree94 <winetree94@gmail.com>
2024-07-05 17:24:15 +0000

    update

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,5 +1,5 @@
-import ko from './translations/ko.json';
 import en from './translations/en.json';
+import ko from './translations/ko.json';
 
 export const LANG_CODE = {
   EN: 'en',
@@ -10,10 +10,50 @@ export const languages = {
   en: {
     name: 'English',
     path: '',
+    header: `
+<link rel="preload" href="/fonts/AgaveNerdFontMono-Regular.ttf" as="font" type="font/woff" crossorigin>
+<link rel="preload" href="/fonts/AgaveNerdFontMono-Bold.ttf" as="font" type="font/woff" crossorigin>
+<style>
+@font-face {
+  font-family: "Agave";
+  src: url("/fonts/AgaveNerdFontMono-Regular.ttf") format("woff");
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: "Agave";
+  src: url("/fonts/AgaveNerdFontMono-Bold.ttf") format("woff");
+  font-weight: 700;
+  font-style: normal;
+  font-display: swap;
+}
+
+body {
+  font-family: "Agave", sans-serif;
+}
+</style>
+    `,
   },
   ko: {
     name: '한국어',
     path: '/ko',
+    header: `
+<link rel="preload" href="/fonts/GowunDodum-Regular.ttf" as="font" type="font/woff" crossorigin>
+<style>
+@font-face {
+  font-family: "Gowun Dodum";
+  src: url("/fonts/GowunDodum-Regular.ttf") format("woff");
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+body {
+  font-family: "Gowun Dodum", sans-serif;
+}
+</style>
+    `,
   },
 } as const;
 
