e2eb8c24a533c488128b1daa609dce6f7f0513b9
winetree94 <winetree94@gmail.com>
2024-07-09 02:32:16 +0900

    update

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,20 +11,20 @@ export const languages = {
     name: 'English',
     path: '',
     header: `
-<link rel="preload" href="/fonts/AgaveNerdFontMono-Regular.ttf" as="font" type="font/woff" crossorigin>
-<link rel="preload" href="/fonts/AgaveNerdFontMono-Bold.ttf" as="font" type="font/woff" crossorigin>
+<link rel="preload" href="/fonts/Agave-Regular.ttf" as="font" type="font/woff" crossorigin>
+<link rel="preload" href="/fonts/Agave-Bold.ttf" as="font" type="font/woff" crossorigin>
 <style>
 @font-face {
   font-family: "Agave";
-  src: url("/fonts/AgaveNerdFontMono-Regular.ttf") format("woff");
+  src: url("/fonts/Agave-Regular.ttf") format("woff");
   font-weight: 400;
   font-style: normal;
-  font-display: swap;
+  font-display: block;
 }
 
 @font-face {
   font-family: "Agave";
-  src: url("/fonts/AgaveNerdFontMono-Bold.ttf") format("woff");
+  src: url("/fonts/Agave-Bold.ttf") format("woff");
   font-weight: 700;
   font-style: normal;
   font-display: swap;
@@ -40,17 +40,36 @@ body {
     name: '한국어',
     path: '/ko',
     header: `
+<link rel="preload" href="/fonts/Agave-Regular.ttf" as="font" type="font/woff" crossorigin>
+<link rel="preload" href="/fonts/Agave-Bold.ttf" as="font" type="font/woff" crossorigin>
 <link rel="preload" href="/fonts/GowunDodum-Regular.ttf" as="font" type="font/woff" crossorigin>
 <style>
+@font-face {
+  font-family: "Agave";
+  src: url("/fonts/Agave-Regular.ttf") format("woff");
+  font-weight: 400;
+  font-style: normal;
+  font-display: block;
+}
+
+@font-face {
+  font-family: "Agave";
+  src: url("/fonts/Agave-Bold.ttf") format("woff");
+  font-weight: 700;
+  font-style: normal;
+  font-display: block;
+}
+
 @font-face {
   font-family: "Gowun Dodum";
   src: url("/fonts/GowunDodum-Regular.ttf") format("woff");
   font-weight: 400;
   font-style: normal;
-  font-display: swap;
+  font-display: block;
 }
+
 body {
-  font-family: "Gowun Dodum", sans-serif;
+  font-family: "Agave", "Gowun Dodum", sans-serif;
 }
 </style>
     `,
