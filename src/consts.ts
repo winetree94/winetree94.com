f0ea8201f78ee2116f5765af11bf7d2b74e76383
winetree94 <winetree94@gmail.com>
2024-07-09 03:25:55 +0900

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,21 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const SITE_TITLES = {
+  en: 'winetree94',
+  ko: 'winetree94',
+};
+
+export const SITE_DESCRIPTIONS = {
+  en: 'winetree94\'s blog',
+  ko: 'winetree94\'s blog',
+};
+
+export const SITE_IMAGES = {
+  en: '/og/en.png',
+  ko: '/og/ko.png',
+};
+
 export const SITE_TITLE = 'winetree94';
 export const SITE_DESCRIPTION = 'winetree94 world';
 export const GITHUB_URL = 'https://github.com/winetree94';
