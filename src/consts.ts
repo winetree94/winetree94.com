de0aa019bf36d69e577c9c0e991c8b2c0b07a7d7
winetree94 <winetree94@gmail.com>
2024-07-11 17:46:15 +0900

    update

--- src/consts.ts
+++ src/consts.ts
@@ -7,8 +7,8 @@ export const SITE_TITLES = {
 };
 
 export const SITE_DESCRIPTIONS = {
-  en: 'winetree94\'s blog',
-  ko: 'winetree94\'s blog',
+  en: "winetree94's blog",
+  ko: "winetree94's blog",
 };
 
 export const SITE_IMAGES = {
@@ -20,5 +20,5 @@ export const SITE_TITLE = 'winetree94';
 export const SITE_DESCRIPTION = 'winetree94 world';
 export const GITHUB_URL = 'https://github.com/winetree94';
 export const NPM_URL = 'https://www.npmjs.com/~winetree94';
-export const LINKEDIN_URL = 'https://www.linkedin.com/in/hansol-park-995380265/';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/winetree94';
 export const EMAIL = 'winetree94@gmail.com';
