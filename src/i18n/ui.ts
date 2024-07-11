bb1ce61218165d1387785cd8fe746771fbc8fa62
winetree94 <winetree94@gmail.com>
2024-07-11 20:01:38 +0900

    update

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -40,36 +40,12 @@ body {
     name: '한국어',
     path: '/ko',
     header: `
-<link rel="preload" href="/fonts/Agave-Regular.ttf" as="font" type="font/woff" crossorigin>
-<link rel="preload" href="/fonts/Agave-Bold.ttf" as="font" type="font/woff" crossorigin>
-<link rel="preload" href="/fonts/GowunDodum-Regular.ttf" as="font" type="font/woff" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
 <style>
-@font-face {
-  font-family: "Agave";
-  src: url("/fonts/Agave-Regular.ttf") format("woff");
-  font-weight: 400;
-  font-style: normal;
-  font-display: block;
-}
-
-@font-face {
-  font-family: "Agave";
-  src: url("/fonts/Agave-Bold.ttf") format("woff");
-  font-weight: 700;
-  font-style: normal;
-  font-display: block;
-}
-
-@font-face {
-  font-family: "Gowun Dodum";
-  src: url("/fonts/GowunDodum-Regular.ttf") format("woff");
-  font-weight: 400;
-  font-style: normal;
-  font-display: block;
-}
-
 body {
-  font-family: "Agave", "Gowun Dodum", sans-serif;
+  font-family: "IBM Plex Sans KR", "Gowun Dodum", sans-serif;
 }
 </style>
     `,
