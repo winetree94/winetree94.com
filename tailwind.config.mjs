bb515109bf736c898d8d2baeeb9809d5caf4dfe1
winetree94 <me@winetree94.com>
2025-11-10 23:34:43 +0900

    refactor: remove the Tailwind tw- prefix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,6 @@ import plugin from 'tailwindcss/plugin';
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	prefix: 'tw-',
 	theme: {
 		colors: {
 			black: {
