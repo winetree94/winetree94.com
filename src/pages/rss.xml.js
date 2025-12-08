4cedc2387da2cdfe6b2c8894171de51cf2259725
winetree94 <me@winetree94.com>
2025-12-09 00:40:24 +0900

    chore: apply ESLint and Prettier

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,18 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const enPosts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: enPosts.map((post) => {
-			return {
-				...post.data,
-				link: `/blog/${post.slug}/`,
-			}
-		}),
-	});
+  const enPosts = await getCollection("blog");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: enPosts.map((post) => {
+      return {
+        ...post.data,
+        link: `/blog/${post.slug}/`,
+      };
+    }),
+  });
 }
