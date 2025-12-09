333d267b5fa7e5c43bb2fb908834111ab4517570
winetree94 <me@winetree94.com>
2025-12-10 00:11:55 +0900

    chore: simplify lint configuration

---
+++ prettier.config.ts
@@ -0,0 +1,21 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: ["**/*.astro"],
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
+
+export default config;
