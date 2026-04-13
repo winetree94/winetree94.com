12d5c478bc496d1b01edd997c87514d4b2cc5933
winetree94 <me@winetree94.com>
2026-04-13 14:38:33 +0900

    refactor(i18n): model locale head links as typed data

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,42 +1,50 @@
+export interface LanguageHeadLink {
+  rel: string;
+  href: string;
+  as?: string;
+  type?: string;
+  crossorigin?: boolean;
+}
+
 export const languageInfos = {
   en: {
-    header: `
-<link rel="preload" href="/fonts/Agave-Regular.ttf" as="font" type="font/woff" crossorigin>
-<link rel="preload" href="/fonts/Agave-Bold.ttf" as="font" type="font/woff" crossorigin>
-<style>
-@font-face {
-  font-family: "Agave";
-  src: url("/fonts/Agave-Regular.ttf") format("woff");
-  font-weight: 400;
-  font-style: normal;
-  font-display: block;
-}
-
-@font-face {
-  font-family: "Agave";
-  src: url("/fonts/Agave-Bold.ttf") format("woff");
-  font-weight: 700;
-  font-style: normal;
-  font-display: swap;
-}
-
-body {
-  font-family: "Agave", sans-serif;
-}
-</style>
-    `,
+    headLinks: [
+      {
+        rel: "preload",
+        href: "/fonts/Agave-Regular.ttf",
+        as: "font",
+        type: "font/woff",
+        crossorigin: true,
+      },
+      {
+        rel: "preload",
+        href: "/fonts/Agave-Bold.ttf",
+        as: "font",
+        type: "font/woff",
+        crossorigin: true,
+      },
+    ],
   },
   ko: {
-    header: `
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="preload" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=block" as="style">
-<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=block" rel="stylesheet">
-<style>
-body {
-  font-family: "IBM Plex Sans KR", "Gowun Dodum", sans-serif;
-}
-</style>
-    `,
+    headLinks: [
+      {
+        rel: "preconnect",
+        href: "https://fonts.googleapis.com",
+      },
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossorigin: true,
+      },
+      {
+        rel: "preload",
+        href: "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=block",
+        as: "style",
+      },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+KR:wght@100;200;300;400;500;600;700&display=block",
+      },
+    ],
   },
 } as const;
