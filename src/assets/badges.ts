4dcd913e755cfa3f1e8f7b2dd9b935144d1d989e
winetree94 <me@winetree94.com>
2026-04-13 17:17:59 +0900

    refactor(assets): move images into src assets

---
+++ src/assets/badges.ts
@@ -0,0 +1,17 @@
+import angularIcon from "@/assets/favicons/angular.png";
+import awsIcon from "@/assets/favicons/aws.png";
+import dockerIcon from "@/assets/favicons/docker.png";
+import jestIcon from "@/assets/favicons/jest.png";
+import reactIcon from "@/assets/favicons/react.png";
+import typescriptIcon from "@/assets/favicons/typescript.png";
+import webpackIcon from "@/assets/favicons/webpack.png";
+
+export const badgeIcons = {
+  angular: angularIcon,
+  aws: awsIcon,
+  docker: dockerIcon,
+  jest: jestIcon,
+  react: reactIcon,
+  typescript: typescriptIcon,
+  webpack: webpackIcon,
+} as const;
